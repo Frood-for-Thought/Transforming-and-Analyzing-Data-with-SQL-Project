52cc91f0a68825a928f12988a1fe58a6805d4b31
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2023-07-08 12:23:53 -0400

    Update read_table_column_name_and_type_function.py

--- read_table_column_name_and_type_function.py
+++ read_table_column_name_and_type_function.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # This program reads files to identify their column parameters as long as the files are in the same folder.
 
+
 def read_csv_or_excel(file_name, suffix):
     """
     This function imports the file contents into a DataFrame and then displays column types and data inside.
@@ -25,4 +26,16 @@ def read_csv_or_excel(file_name, suffix):
 # Input the file name and extension (suffix) display the results described in the function "read_csv_or_excel"
 file_name = input("input the exact file name to read: ")
 suffix = input("Type 'c' for .csv file or 'x' for excel .xlsx: ")
-print(read_csv_or_excel(file_name, suffix))
+
+# Get a series list with the column names and data types (file_df_dtypes),
+# and a sample of the first 5 rows for the table, (file_df_head)
+file_df_dtypes, file_df_head = read_csv_or_excel(file_name, suffix)
+column_list = list(file_df_dtypes.index)
+
+print(f"\nTable First 5 Rows:\n{file_df_head}")  # Print off a sample of the table's first 5 rows
+
+print(f"\nColumn Names and Their Datatypes:\n{file_df_dtypes}")  # Print off a list of the column names and their datatypes
+
+print("\nColumn Names in Lowercase:")
+for i, j in enumerate(column_list):
+    print(j.lower())  # Print off a list of all the column names in lowercase.
