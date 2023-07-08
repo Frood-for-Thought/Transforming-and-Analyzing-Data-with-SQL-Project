f221e8fd61c845301ea91d674f3d5b5123c6f0c4
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2023-07-07 21:29:15 -0400

    Update read_table_column_name_and_type_function.py

--- read_table_column_name_and_type_function.py
+++ read_table_column_name_and_type_function.py
@@ -5,11 +5,9 @@ import numpy as np
 def read_csv_or_excel(file_name, suffix):
     if suffix.lower() == 'c':
         dataframe = pd.read_csv(f"{file_name}.csv")
-        pd.set_option('display.max_columns', None)  # Show all the columns
         return dataframe.dtypes
     elif suffix.lower() == 'x':
         dataframe = pd.read_excel(f"{file_name}.xlsx")
-        pd.set_option('display.max_columns', None)  # Show all the columns
         return dataframe.dtypes
     else:
         print("cannot read file")
